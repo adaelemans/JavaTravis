41811e68bbf2970404163bf257c5ce5ce45fcfd2
Austin Daelemans <austin.daelemans@b-yond.com>
2018-02-08 09:27:24 -0600

    Added Test Cases

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 import unittest
 
+
+
+
 def fun(x):
 	return x + 1
 
 class MyTest(unittest.TestCase):
+	lis = [x for x in range(10) if x % 2 == 0]
 	def test(self):
 		self.assertEqual(fun(3), 4)
+
+	def test2(self):
+		self.assertEqual(sum(lis), 20)
+
+testSuite = MyTest()
+
+testSuite.test2()
