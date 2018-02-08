f6e33c25515b0b2c90c34cc8e65a8fefb07991bd
Austin Daelemans <austin.daelemans@b-yond.com>
2018-02-08 11:48:12 -0600

    Changed Tests and added coverage

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
-def fun(x):
-	return x + 1
-
 lis = [x for x in range(10) if x % 2 == 0]
-def test():
-	if not fun(3) == 4:
-		raise AssertionError()
 
-def test2():
+def test():
 	if not sum(lis) == 20:
 		raise AssertionError()
