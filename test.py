ae910b36fc8d42ad1c2d7307c6c38ec4627534ad
Austin Daelemans <austin.daelemans@b-yond.com>
2018-02-08 09:34:34 -0600

    Reconfigured for py.test

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
-import unittest
-
 def fun(x):
 	return x + 1
 
-class MyTest(unittest.TestCase):
-	lis = [x for x in range(10) if x % 2 == 0]
-	def test(self):
-		self.assertEqual(fun(3), 4)
+lis = [x for x in range(10) if x % 2 == 0]
+def test():
+	assert fun(3) == 4
 
-	def test2(self):
-		self.assertEqual(sum(lis), 20)
+def test2():
+	assert sum(lis) == 20
